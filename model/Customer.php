fc70f8d17347b162a23041b1d07ac58c1493a477
yollovi <125940189+yollovi@users.noreply.github.com>
2023-11-21 10:03:29 +0700

    Update Customer.php
    nambahin connection di construct,klo ngaco apus aja :v

--- model/Customer.php
+++ model/Customer.php
@@ -7,12 +7,21 @@
         private string $customer_dob;
         private string $customer_money;
 
+        // editan vincent klo ngaco blg aja
+        private $conn;
+        protected $db;
+
         public function __construct($user_email, $user_password, $user_type, $customer_first_name, $customer_last_name, $customer_dob, $customer_money) {
             parent::__construct($user_email, $user_password, $user_type);
             $this->customer_first_name = $customer_first_name;
             $this->customer_last_name = $customer_last_name;
             $this->customer_dob = $customer_dob;
             $this->customer_money = $customer_money;
+
+            // ni juga
+            $this->conn = Connect::getInstance();
+            $this->db =  $this->conn->getDBConnection();
+            
         }
 
         public function insert() {
