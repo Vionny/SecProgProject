b86ccaeebcd6fe8ef0d4e7768d3691ebcae97953
Len <89083544+vlenv@users.noreply.github.com>
2023-11-21 18:37:39 +0700

    fix some

--- view/transactionlist.php
+++ view/transactionlist.php
@@ -1,106 +1,97 @@
 <?php
-session_start();
+    session_start();
 
-// Function to display the transaction list
-function displayTransactionList($savedCarts) {
-    echo "<h2>Transaction List</h2>";
+    function displayTransactionList($savedCarts) {
+        echo "<h2>Transaction List</h2>";
 
-    if (empty($savedCarts)) {
-        echo "No transactions available.";
-    } else {
-        echo "<table border='1'>
-            <tr>
-                <th>Transaction ID</th>
-                <th>Customer ID</th>
-                <th>Seller ID</th>
-                <th>Transaction Date</th>
-                <th>Action</th>
-            </tr>";
+        if (empty($savedCarts)) {
+            echo "No transactions available.";
+        } else {
+            echo "<table border='1'>
+                <tr>
+                    <th>Transaction ID</th>
+                    <th>Customer ID</th>
+                    <th>Seller ID</th>
+                    <th>Transaction Date</th>
+                    <th>Action</th>
+                </tr>";
 
-        foreach ($savedCarts as $transactionId => $transaction) {
-            // Assume transaction details are retrieved from a database or other source
-            // For simplicity, using placeholder values
-            $customerId = '123'; // Replace with actual customer ID
-            $sellerId = '456'; // Replace with actual seller ID
+            foreach ($savedCarts as $transactionId => $transaction) {
+                $customerId = 'CU1'; 
+                $sellerId = 'SE1'; 
 
-            echo "<tr>
-                <td>$transactionId</td>
-                <td>$customerId</td>
-                <td>$sellerId</td>
-                <td>";
+                echo "<tr>
+                    <td>$transactionId</td>
+                    <td>$customerId</td>
+                    <td>$sellerId</td>
+                    <td>";
 
-            // Check if the 'transaction_date' key is present in the current transaction
-            if (isset($transaction['transaction_date'])) {
-                // Convert the stored date to WIB timezone
-                $transactionDate = new DateTime($transaction['transaction_date'], new DateTimeZone('UTC'));
-                $transactionDate->setTimezone(new DateTimeZone('Asia/Jakarta'));
-                echo $transactionDate->format('Y-m-d H:i:s');
-            } else {
-                echo "N/A"; // Replace with a suitable message or leave it blank
+                if (isset($transaction['transaction_date'])) {
+                    $transactionDate = new DateTime($transaction['transaction_date'], new DateTimeZone('UTC'));
+                    $transactionDate->setTimezone(new DateTimeZone('Asia/Jakarta'));
+                    echo $transactionDate->format('Y-m-d H:i:s');
+                } else {
+                    echo "-";
+                }
+
+                echo "</td>
+                    <td><a href='view_transaction.php?transaction_id=$transactionId'>View Details</a></td>
+                    <td><a href='transactionlist.php?delete_transaction=$transactionId'>Delete</a></td>
+                </tr>";
             }
 
-            // Add links for view and delete actions
-            echo "</td>
-                <td><a href='view_transaction.php?transaction_id=$transactionId'>View Details</a></td>
-                <td><a href='transactionlist.php?delete_transaction=$transactionId'>Delete</a></td>
-            </tr>";
+            echo "</table>";
         }
-
-        echo "</table>";
     }
-}
 
-// Function to delete a specific transaction
-function deleteTransaction($transactionId) {
-    if (isset($_SESSION['saved_carts'][$transactionId])) {
-        unset($_SESSION['saved_carts'][$transactionId]);
-        echo "Transaction $transactionId has been deleted.";
-    } else {
-        echo "Transaction not found.";
+    function deleteTransaction($transactionId) {
+        if (isset($_SESSION['saved_carts'][$transactionId])) {
+            unset($_SESSION['saved_carts'][$transactionId]);
+            echo "Transaction $transactionId has been deleted.";
+        } else {
+            echo "Transaction not found.";
+        }
     }
-}
 
-// Check if a delete action is triggered
-if (isset($_GET['delete_transaction'])) {
-    $transactionIdToDelete = $_GET['delete_transaction'];
-    deleteTransaction($transactionIdToDelete);
-}
+    if (isset($_GET['delete_transaction'])) {
+        $transactionIdToDelete = $_GET['delete_transaction'];
+        deleteTransaction($transactionIdToDelete);
+    }
 
-if (!isset($_SESSION['saved_carts'])) {
-    $_SESSION['saved_carts'] = array();
-}
+    if (!isset($_SESSION['saved_carts'])) {
+        $_SESSION['saved_carts'] = array();
+    }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Transaction List</title>
-    <style>
-        /* Add your styles here */
-        table {
-            border-collapse: collapse;
-            width: 100%;
-        }
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Transaction List</title>
+        <style>
+            table {
+                border-collapse: collapse;
+                width: 100%;
+            }
 
-        th, td {
-            border: 1px solid #ddd;
-            padding: 8px;
-            text-align: left;
-        }
+            th, td {
+                border: 1px solid #ddd;
+                padding: 8px;
+                text-align: left;
+            }
 
-        th {
-            background-color: #f2f2f2;
-        }
-    </style>
-</head>
-<body>
-    <div>
-        <a href="../index.php">Back to Store</a>
-    </div>
-    <div>
-        <?php displayTransactionList($_SESSION['saved_carts']); ?>
-    </div>
-</body>
+            th {
+                background-color: #f2f2f2;
+            }
+        </style>
+    </head>
+    <body>
+        <div>
+            <a href="../index.php">Back to Store</a>
+        </div>
+        <div>
+            <?php displayTransactionList($_SESSION['saved_carts']); ?>
+        </div>
+    </body>
 </html>
--- view/view_transaction.php
+++ view/view_transaction.php
@@ -1,80 +1,72 @@
 <?php
-session_start();
+    session_start();
 
-// Function to display transaction details
-function displayTransactionDetails($transactionId, $savedCarts) {
-    echo "<h2>Transaction Details</h2>";
+    function displayTransactionDetails($transactionId, $savedCarts) {
+        echo "<h2>Transaction Details</h2>";
 
-    // Check if the selected transaction ID exists in saved carts
-    if (isset($savedCarts[$transactionId])) {
-        $transaction = $savedCarts[$transactionId]['cart'];
-        
-        echo "<table border='1'>
-            <tr>
-                <th>Item ID</th>
-                <th>Item Name</th>
-                <th>Item Price</th>
-                <th>Quantity</th>
-            </tr>";
+        if (isset($savedCarts[$transactionId])) {
+            $transaction = $savedCarts[$transactionId]['cart'];
+            
+            echo "<table border='1'>
+                <tr>
+                    <th>Item ID</th>
+                    <th>Item Name</th>
+                    <th>Item Price</th>
+                    <th>Quantity</th>
+                </tr>";
 
-        foreach ($transaction as $itemId => $quantity) {
-            // Assume item details are retrieved from a database or other source
-            // For simplicity, using placeholder values
-            $itemName = "Product $itemId";
-            $itemPrice = 10 + $itemId * 5;
+            foreach ($transaction as $itemId => $quantity) {
+                $itemName = "Product $itemId";
+                $itemPrice = 10 + $itemId * 5;
 
-            echo "<tr>
-                <td>$itemId</td>
-                <td>$itemName</td>
-                <td>\$$itemPrice</td>
-                <td>$quantity</td>
-            </tr>";
+                echo "<tr>
+                    <td>$itemId</td>
+                    <td>$itemName</td>
+                    <td>\$$itemPrice</td>
+                    <td>$quantity</td>
+                </tr>";
+            }
+
+            echo "</table>";
+        } else {
+            echo "Transaction not found.";
         }
-
-        echo "</table>";
-    } else {
-        echo "Transaction not found.";
     }
-}
 
-// Check if the transaction_id is set in the query parameters
-if (isset($_GET['transaction_id'])) {
-    $transactionId = $_GET['transaction_id'];
-
-    // Call the function to display transaction details
-    displayTransactionDetails($transactionId, $_SESSION['saved_carts']);
-} else {
-    echo "Invalid request. Please provide a valid transaction ID.";
-}
+    if (isset($_GET['transaction_id'])) {
+        $transactionId = $_GET['transaction_id'];
+        displayTransactionDetails($transactionId, $_SESSION['saved_carts']);
+    } else {
+        echo "Invalid request. Please provide a valid transaction ID.";
+    }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>View Transaction</title>
-    <style>
-        /* Add your styles here */
-        table {
-            border-collapse: collapse;
-            width: 100%;
-        }
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>View Transaction</title>
+        <style>
+            table {
+                border-collapse: collapse;
+                width: 100%;
+            }
 
-        th, td {
-            border: 1px solid #ddd;
-            padding: 8px;
-            text-align: left;
-        }
+            th, td {
+                border: 1px solid #ddd;
+                padding: 8px;
+                text-align: left;
+            }
 
-        th {
-            background-color: #f2f2f2;
-        }
-    </style>
-</head>
-<body>
-    <div>
-        <a href="transactionlist.php">Back to Transaction List</a>
-    </div>
-</body>
+            th {
+                background-color: #f2f2f2;
+            }
+        </style>
+    </head>
+    <body>
+        <div>
+            <a href="transactionlist.php">Back to Transaction List</a>
+        </div>
+    </body>
 </html>
